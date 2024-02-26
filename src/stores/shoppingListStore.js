059d19c286a868076c3b85e4845df9edc23b902e
Tom Lorius <tomlorius.mail@gmail.com>
2024-02-26 23:08:00 +0100

    initial commit

---
+++ src/stores/shoppingListStore.js
@@ -0,0 +1,29 @@
+import { defineStore } from "pinia";
+
+export const useShoppingListStore = defineStore("shoppingList", {
+  state: () => ({
+    groceries: JSON.parse(localStorage.getItem("shoppingList")) || [],
+  }),
+  actions: {
+    addItem(item) {
+      this.groceries.push(item);
+      this.saveToLocalStorage();
+    },
+    removeItem(itemId) {
+      this.groceries = this.groceries.filter((item) => item.id !== itemId);
+      this.saveToLocalStorage();
+    },
+    updateItem(updatedItem) {
+      const index = this.groceries.findIndex(
+        (item) => item.id === updatedItem.id
+      );
+      if (index !== -1) {
+        this.groceries[index] = updatedItem;
+        this.saveToLocalStorage();
+      }
+    },
+    saveToLocalStorage() {
+      localStorage.setItem("shoppingList", JSON.stringify(this.groceries));
+    },
+  },
+});
---
+++ src/views/ShoppingListView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Shopping List rendered here</h1>
+  </div>
+</template>
