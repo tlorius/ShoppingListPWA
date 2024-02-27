2a546e21f7e1f138841ce3fde6d1a02b155ad5d0
Tom Lorius <tomlorius.mail@gmail.com>
2024-02-27 15:24:56 +0100

    feat: completed data layout for components

--- src/views/ShoppingListView.vue
+++ src/views/ShoppingListView.vue
@@ -1,17 +1,13 @@
 <script setup>
 import AddItemForm from '@/components/AddItemForm.vue';
-import ShoppingListItem from '@/components/ShoppingListItem.vue';
-import { useShoppingListStore } from '@/stores/shoppingListStore';
+import ShoppingList from '@/components/ShoppingList.vue';
 
-const store = useShoppingListStore()
 </script>
 
 <template>
-  <div class="about">
-    <h1>Shopping List rendered here</h1>
     <v-container>
-        <ShoppingListItem v-for="item in store.groceries" :key="item.id" :item="item"/>
+        <AddItemForm />
+        <ShoppingList />
     </v-container>
-    <AddItemForm />
-  </div>
+    
 </template>
