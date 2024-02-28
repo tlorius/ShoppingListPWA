620da77d12b14e8441eec919aec4e755b1aa10f6
Tom Lorius <tomlorius.mail@gmail.com>
2024-02-28 15:24:53 +0100

    feat: basic pwa setup

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import { createVuetify } from "vuetify";
 import { aliases, mdi } from "vuetify/iconsets/mdi";
 import * as components from "vuetify/components";
 import * as diretives from "vuetify/directives";
+import './registerServiceWorker'
 
 const vuetify = createVuetify({
   components,
